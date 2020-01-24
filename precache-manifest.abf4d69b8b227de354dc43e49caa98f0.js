86e8fe0e8ac8d726e36135c8b953c853da716467
thatsitnothing <thatsitnothing@gmail.com>
2020-01-24 15:53:45 -0800

    Updates

---
+++ precache-manifest.abf4d69b8b227de354dc43e49caa98f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdb50c070fcf47ace3c6a44c022a33f",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "87af37a5054bfc53f847",
+    "url": "/shop/static/css/main.1ce87045.chunk.css"
+  },
+  {
+    "revision": "dd0ff79f02d16332d927",
+    "url": "/shop/static/js/2.706088de.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shop/static/js/2.706088de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87af37a5054bfc53f847",
+    "url": "/shop/static/js/main.03f83340.chunk.js"
+  },
+  {
+    "revision": "c1425516936ef75a4df6",
+    "url": "/shop/static/js/runtime-main.b6aa5fd3.js"
+  }
+]);
